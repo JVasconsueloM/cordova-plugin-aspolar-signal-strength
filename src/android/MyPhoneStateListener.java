92a44f736b81ed06eb329a09c2c5c2a0e72f14e9
Polar <jvasconsuelom@gmail.com>
2018-09-29 00:58:23 -0500

    fix

--- src/android/MyPhoneStateListener.java
+++ src/android/MyPhoneStateListener.java
@@ -1,31 +1,32 @@
-/**
- * author :  lipan
- * filename :  Snippet.java
- * create_time : 2014年11月12日 下午4:44:22
- */
 package org.apache.cordova.aspolarsignalstrength;
 
 import android.content.Context;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 
-/**
- * @author : lipan
- * @create_time : 2014年11月12日 下午4:44:22
- * @desc : 手机状态变化监控
- * @update_person:
- * @update_time :
- * @update_desc :
- *
- */
-public class MyPhoneStateListener2 extends PhoneStateListener
+
+
+public class MyPhoneStateListener extends PhoneStateListener {
+    int signalLevel;
+
+    @Override
+    public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+        super.onSignalStrengthsChanged(signalStrength);
+
+        signalLevel = signalStrength.getGsmSignalStrength();
+    }
+
+}
+
+
+/*public class MyPhoneStateListener extends PhoneStateListener
 {
     private Context context;
-    public  MyPhoneStateListener2(Context context)
+    public  MyPhoneStateListener(Context context)
     {
         this.context = context;
     }
-    private Integer signalNum = -1; //信号强度
+    private Integer signalLevel = -1;
     
     
     @Override
@@ -39,20 +40,20 @@ public class MyPhoneStateListener2 extends PhoneStateListener
             {
                 String ssignal = signalStrength.toString();
                 String[] parts = ssignal.split(" ");
-                signalNum = Integer.parseInt(parts[11]);
+                signalLevel = Integer.parseInt(parts[11]);
             } else
             {
-                signalNum = (signalStrength.getGsmSignalStrength() * 2) - 113;
+                signalLevel = (signalStrength.getGsmSignalStrength() * 2) - 113;
             }
         } catch (Exception e)
         {
-            signalNum = -1;
+            signalLevel = -1;
         }
     }
     
     
     public  Integer getSignalNum()
     {
-        return signalNum;
+        return signalLevel;
     }
-}
+}*/
