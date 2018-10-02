770160ee219075c90055a1d5059c1f4ed0f3c5ea
JvasconsueloM <jvasconsuelom@gmail.com>
2018-10-02 18:25:33 -0500

    add permissions for android > 5, and add get percentage and dbm

--- src/android/SignalStrength.java
+++ src/android/SignalStrength.java
@@ -13,10 +13,11 @@ import android.telephony.CellSignalStrengthWcdma;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
+import org.apache.cordova.PermissionHelper;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,80 +28,48 @@ import java.util.List;
 
 
 public class SignalStrength extends CordovaPlugin {
-    int dBmlevel = 0;
     int asulevel = 0;
+    int asulevelmax = 31;
+    int dBmlevel = 0;
     int signalLevel = -1;
+    String message = "";
+    double signalpercentage = 0.0;
     TelephonyManager tm;
+
+    public static final int CONTINUE = 1;
+    protected final static String[] permissions = { Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_COARSE_LOCATION };
 
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        if (!action.equals("getInfo")) {
+        if (!action.equals("getbBm") && !action.equals("getPercentage")) {
             return false;
         }
 
         tm = (TelephonyManager)cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
 
         try {
-            List<CellInfo> cellInfoList = tm.getAllCellInfo();
-            //Checking if list values are not null
-            if (cellInfoList != null) {
-                for (final CellInfo info : cellInfoList) {
-                    if (info instanceof CellInfoGsm) {
-                        //GSM Network
-                        CellSignalStrengthGsm cellSignalStrength = ((CellInfoGsm)info).getCellSignalStrength();
-                        dBmlevel = cellSignalStrength.getDbm();
-                        asulevel = cellSignalStrength.getAsuLevel();
-                    }
-                    else if (info instanceof CellInfoCdma) {
-                        //CDMA Network
-                        CellSignalStrengthCdma cellSignalStrength = ((CellInfoCdma)info).getCellSignalStrength();
-                        dBmlevel = cellSignalStrength.getDbm();
-                        asulevel = cellSignalStrength.getAsuLevel();
-                    }
-                    else if (info instanceof CellInfoLte) {
-                        //LTE Network
-                        CellSignalStrengthLte cellSignalStrength = ((CellInfoLte)info).getCellSignalStrength();
-                        dBmlevel = cellSignalStrength.getDbm();
-                        asulevel = cellSignalStrength.getAsuLevel();
-                    }
-                    else if  (info instanceof CellInfoWcdma) {
-                        //WCDMA Network
-                        CellSignalStrengthWcdma cellSignalStrength = ((CellInfoWcdma)info).getCellSignalStrength();
-                        dBmlevel = cellSignalStrength.getDbm();
-                        asulevel = cellSignalStrength.getAsuLevel();
-                    }
-                    else{
-                        // callbackContext.error("Unknown type of cell signal.");
-                        Log.i("tag", "Unknown type of cell signal.");
-                        return false;
-                    }
-                }
+
+            if(!PermissionHelper.hasPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                PermissionHelper.requestPermission(this, CONTINUE, Manifest.permission.ACCESS_COARSE_LOCATION);
             }
             else{
-                //Mostly for Samsung devices, after checking if the list is indeed empty.
-                MyPhoneStateListener MyListener = new MyPhoneStateListener();
-                tm.listen(MyListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
-                int cc = 0;
-                while ( signalLevel == -1){
-                    Thread.sleep(200);
-                    if (cc++ >= 5)
-                    {
-                        break;
-                    }
-                }
-                asulevel = signalLevel;
-                dBmlevel = -113 + 2 * asulevel;
-                tm.listen(MyListener, PhoneStateListener.LISTEN_NONE);
-                signalLevel = -1;
+                this.processInfo();
             }
+
         }
         catch (Exception ex){
-            // callbackContext.error("Failed to retrieve signal strength.");
+            callbackContext.error("Failed to retrieve signal strength. (" + ex + ")");
             Log.i("tag", "Failed to retrieve signal strength.", ex);
             return false;
         }
 
-        String message = "Signal strength: " + dBmlevel + " dBm, "+ asulevel + " asu";
+        message = dBmlevel  + "";
+
+        if (action.equals("getPercentage")) {
+            this.getPercentage();
+        }
+
+
         callbackContext.success(message);
         return true;
         
@@ -114,7 +83,118 @@ public class SignalStrength extends CordovaPlugin {
             super.onSignalStrengthsChanged(signalStrength);
             signalLevel = signalStrength.getGsmSignalStrength();
         }
+    }
+
+    //--------------------------------------------------------------------------
+    // LOCAL METHODS
+    //--------------------------------------------------------------------------
+
+    /**         MIN     MAX 
+    CDMA (2g)   
+        dBm =   -100    -75
+        asu =   1       16
+    GSM (3g)
+        dBm =   -120    -50
+        asu =   0(99)   31
+    WCDMA(3g)
+        dBm =   -115    -50
+        asu =   0(99)   31
+    LTE (4g)
+        dBm =   -140    -44
+        asu =   0(99)   97
+
+    LINEAR =        100 * (1 - (((-dBmmax) - (-dBmlevel))/((-dBmmax) - (-dBmmin))));
+    NOT LINEAR =    (2.0 * (dBmlevel + 100))/100
+
+    */
+    public void getPercentage(String networkType){
+        switch (networkType) {
+            case 'wifi':
+                signalpercentage = (2.0 * (dBmlevel + 100))/100;
+                break;
+            default:
+                signalpercentage = asulevel/asulevelmax * 1.0;
+                break;
+        }
+        message = signalpercentage + "";
+        Log.i("tag", message);
 
     }
+
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+        for (int r : grantResults) {
+            if (r == PackageManager.PERMISSION_DENIED) {
+                this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "PERMISSION_DENIED_ERROR"));
+                return;
+            }
+        }
+        switch (requestCode) {
+            case CONTINUE:
+                this.processInfo(this.srcType, this.destType, this.encodingType);
+                break;
+        }
+    }
+
+
+    public void processInfo(){
+        List<CellInfo> cellInfoList = tm.getAllCellInfo();
+        //Checking if list values are not null
+        if (cellInfoList != null) {
+            for (final CellInfo info : cellInfoList) {
+                if (info instanceof CellInfoGsm) {
+                    //GSM Network
+                    CellSignalStrengthGsm cellSignalStrength = ((CellInfoGsm)info).getCellSignalStrength();
+                    dBmlevel = cellSignalStrength.getDbm();
+                    asulevel = cellSignalStrength.getAsuLevel();
+                    asulevelmax = 31;
+                }
+                else if (info instanceof CellInfoCdma) {
+                    //CDMA Network
+                    CellSignalStrengthCdma cellSignalStrength = ((CellInfoCdma)info).getCellSignalStrength();
+                    dBmlevel = cellSignalStrength.getDbm();
+                    asulevel = cellSignalStrength.getAsuLevel();
+                    asulevelmax = 16;
+                }
+                else if (info instanceof CellInfoLte) {
+                    //LTE Network
+                    CellSignalStrengthLte cellSignalStrength = ((CellInfoLte)info).getCellSignalStrength();
+                    dBmlevel = cellSignalStrength.getDbm();
+                    asulevel = cellSignalStrength.getAsuLevel();
+                    asulevelmax = 97;
+                }
+                else if  (info instanceof CellInfoWcdma) {
+                    //WCDMA Network
+                    CellSignalStrengthWcdma cellSignalStrength = ((CellInfoWcdma)info).getCellSignalStrength();
+                    dBmlevel = cellSignalStrength.getDbm();
+                    asulevel = cellSignalStrength.getAsuLevel();
+                    asulevelmax = 31;
+                }
+                else{
+                    callbackContext.error("Unknown type of cell signal.");
+                    Log.i("tag", "Unknown type of cell signal.");
+                    return false;
+                }
+            }
+        }
+        else{
+            //Mostly for Samsung devices, after checking if the list is indeed empty.
+            MyPhoneStateListener MyListener = new MyPhoneStateListener();
+            tm.listen(MyListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+            int cc = 0;
+            while ( signalLevel == -1){
+                Thread.sleep(200);
+                if (cc++ >= 5)
+                {
+                    break;
+                }
+            }
+            asulevel = signalLevel;
+            dBmlevel = -113 + 2 * asulevel;
+            tm.listen(MyListener, PhoneStateListener.LISTEN_NONE);
+            signalLevel = -1;
+        }
+    }
+
 } 
 
--- www/SignalStrength.js
+++ www/SignalStrength.js
@@ -1,19 +1,28 @@
 var exec = require('cordova/exec');
+var argscheck = require('cordova/argscheck');
 
 function SignalStrength() {}
 
-SignalStrength.prototype.getInfo = function (successCallback, errorCallback) {
-  exec(successCallback, errorCallback, 'SignalStrength', 'getInfo', []);
+SignalStrength.prototype.getbBm = function (successCallback, errorCallback) {
+  	exec(successCallback, errorCallback, 'SignalStrength', 'getbBm', []);
+};
+
+SignalStrength.prototype.getPercentage = function (successCallback, errorCallback, options) {
+	options = options || {};
+    var getValue = argscheck.getValue;
+    var typeNetwork = getValue(options.typeNetwork, 'wifi');
+	args = [typeNetwork];
+  	exec(successCallback, errorCallback, 'SignalStrength', 'getPercentage', args);
 };
 
 SignalStrength.install = function () {
-  if (!window.plugins) {
-    window.plugins = {};
-  }
+  	if (!window.plugins) {
+    	window.plugins = {};
+  	}
 
 
-  window.plugins.signalStrength = new SignalStrength();
-  return window.plugins.signalStrength;
+  	window.plugins.signalStrength = new SignalStrength();
+  	return window.plugins.signalStrength;
 };
 
 cordova.addConstructor(SignalStrength.install);
